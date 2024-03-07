2c44f43f88a381dfc469e347652887dc7621dc93
Yuliya Ivanova <70429139+trdgrd@users.noreply.github.com>
2024-03-07 23:10:53 +0300

    add first project files

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -18,8 +18,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
 
-    private static Random random = new Random();
     private Burger burger;
+    private static Random random = new Random();
     private String randomName = RandomStringUtils.randomAlphabetic(6);
     private Float randomPrice = random.nextFloat();
 
@@ -69,6 +69,37 @@ public class BurgerTest {
         burger.moveIngredient(0, 1);
         assertEquals(1, burger.ingredients.indexOf(firstIngredient));
         assertEquals(0, burger.ingredients.indexOf(secondIngredient));
+    }
+
+    @Test
+    public void getPriceTest() {
+        burger.setBuns(bun);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        Mockito.when(bun.getPrice()).thenReturn(randomPrice);
+        Mockito.when(firstIngredient.getPrice()).thenReturn(randomPrice);
+        Mockito.when(secondIngredient.getPrice()).thenReturn(randomPrice);
+        float expected = randomPrice * 4;
+        assertEquals(expected, burger.getPrice());
+    }
+
+    @Test
+    public void getReceiptTest() {
+        burger.setBuns(bun);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        Mockito.when(bun.getName()).thenReturn("black bun");
+        Mockito.when(firstIngredient.getName()).thenReturn("cutlet");
+        Mockito.when(secondIngredient.getName()).thenReturn("mustard");
+        Mockito.when(firstIngredient.getType()).thenReturn(IngredientType.FILLING);
+        Mockito.when(secondIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(burger.getPrice()).thenReturn(randomPrice);
+        String expected = "(==== black bun ====)\n" +
+                "= filling cutlet =\n" +
+                "= sauce mustard =\n" +
+                "(==== black bun ====)" +
+                String.format("\n\nPrice: %f%n", randomPrice);
+        assertEquals(expected, burger.getReceipt());
     }
 
 }
